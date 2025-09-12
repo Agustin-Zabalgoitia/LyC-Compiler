e4fc0ccdfaff56c0e39e1c4c1ed4f38ac0f37ea7
Agustin-Zabalgoitia <aguszabalgoitia@gmail.com>
2025-09-12 20:13:29 -0300

    Added SymbolTableGenerator code

--- src/main/java/lyc/compiler/files/SymbolTableGenerator.java
+++ src/main/java/lyc/compiler/files/SymbolTableGenerator.java
@@ -3,10 +3,18 @@ package lyc.compiler.files;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class SymbolTableGenerator implements FileGenerator{
+import lyc.compiler.SymbolTable;
+
+public class SymbolTableGenerator implements FileGenerator {
 
     @Override
     public void generate(FileWriter fileWriter) throws IOException {
-        fileWriter.write("TODO");
+        SymbolTable st = SymbolTable.getSymbolTable();
+        fileWriter.write("Nombre, TipoDato, Valor, Longitud\n");
+        for (String[] row : st.getTable()) {
+            for (int i = 0; i < row.length; i++)
+                fileWriter.write(String.format("%s, ", row[i]));
+            fileWriter.write("\n");
+        }
     }
 }
